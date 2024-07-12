be984c12b87e06a96a298d7b4b61eb8fe5840989
James <jmsn311@gmail.com>
2024-07-12 12:06:41 -0600

    added components

--- my-app/src/components/Trim.js
+++ my-app/src/components/Trim.js
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
+import { React, useState, useEffect } from "react";
+import myImage from '../assets/61.jpeg'; 
+
 
 function Trim () {
+    const [trims, setTrims] = useState([]);
+    const [selectedTrim, setSelectedTrim] = useState(null);
+
+
+    useEffect(() => {
+        fetch('http://localhost:4000/parts')
+            .then((response) => response.json())
+            .then((data) => setTrims(data))
+            .catch((error) => console.error('Error fetching trims:', error));
+    }, []);
     
+    const handleTrimClick = (trim) => {
+        setSelectedTrim(trim);
+    };
 
     return(
         <div>
-            <></>
+            <h2>Trim</h2>
+            <img src={myImage} className="center" alt="Primed" />
+            <ul>
+                {trims.map((trim) => (
+                    <li key={trim.id} onClick={() => handleTrimClick(trim)}>
+                        {trim.name}
+                    </li>
+                ))}
+            </ul>
+            {selectedTrim && (
+                <div>
+                    <h3>Selected Trim: {selectedTrim.name}</h3>
+                    <p>Price: ${selectedTrim["comparison price"]}</p>
+                </div>
+            )}
         </div>
     )
 }
 
 
 export default Trim;
+
